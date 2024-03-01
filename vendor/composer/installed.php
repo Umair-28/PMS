0720ca7e1f0f7071c49142f188087323f6d30b4c
umair.amjad <umair.amjad@Muhammad-Umair-Amjad.local>
2024-03-01 16:31:36 +0500

    Project is Dockerized

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'laravel/laravel',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => null,
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'cac9b1c4cc773a76510f5fb2a7f5ac3829dd4116',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -380,9 +380,9 @@
             'dev_requirement' => false,
         ),
         'laravel/laravel' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => null,
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'cac9b1c4cc773a76510f5fb2a7f5ac3829dd4116',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
